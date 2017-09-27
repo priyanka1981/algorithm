c2330b43eefc54393dfc41f6cd5163e68381b0bd
Priyanka Sharma <priyankasharma181@gmail.com>
2017-09-27 16:48:53 -0400

    Comit program file

--- Algorithms/Program.cs
+++ Algorithms/Program.cs
@@ -12,16 +12,22 @@ namespace Algorithms
         static void Tree()
         {
             Algorithms.Tree.Node<int> root = new Algorithms.Tree.Node<int>();
-            root.setValue(2);
-           
+            root.setValue(3);
+            root.setIs_root(true); 
             Algorithms.Tree.Node<int> left = new Algorithms.Tree.Node<int>();
-            left.setValue(1);
+            left.setValue(2);
             Algorithms.Tree.Tree<int> tree = new Algorithms.Tree.Tree<int>(root);
+			Algorithms.Tree.Node<int> left_left = new Algorithms.Tree.Node<int>();
+			left.setValue(1);
             tree.addChild(left);
+			tree.addChild(left_left);
 			Algorithms.Tree.Node<int> right = new Algorithms.Tree.Node<int>();
 			right.setValue(4);
             tree.addChild(right);
-            tree.inOrderTraversal(root);
+            tree.printTree("in");
+			tree.printTree("pre");
+            tree.printTree("post");
         }
+		
     }
 }
